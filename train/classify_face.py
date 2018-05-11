ffcd05488b0d3ca75efa1334769148b2c980ec90
El-Chiang <jing1downey@gmail.com>
2018-05-12 02:05:22 +0800

    封装了ClassificationModel类

--- train/classify_face.py
+++ train/classify_face.py
@@ -61,74 +61,96 @@ def reshape_img():
         img.resize((64, 64), Image.ANTIALIAS).save(img_name)
 
 
-def classfication_model():
-    batch_size = 10
-    nb_classes = 6  # total number of classes
-    nb_epoch = 16
+class ClassificationModel:
+    def __init__(self, batch_size=10, nb_classes=6, nb_epoch=16, nb_filters=32, nb_pool=2, nb_conv=3):
+        self.batch_size = batch_size
+        self.nb_classes = nb_classes  # total number of classes
+        self.nb_epoch = nb_epoch
 
-    nb_filters = 32  # number of convolutional filter
-    nb_pool = 2  # size of pooling area for max pooling
-    nb_conv = 3  # convolution kernal size
+        self.nb_filters = nb_filters  # number of convolutional filter
+        self.nb_pool = nb_pool  # size of pooling area for max pooling
+        self.nb_conv = nb_conv  # convolution kernal size
 
-    (X_train, Y_train, X_valid, Y_valid, X_test, Y_test) = split_data('train_faces.pkl')
-    X_train = X_train.reshape(X_train.shape[0], 1, 64, 64)
-    X_test = X_test.reshape(X_test.shape[0], 1, 64, 64)
-    print('X_train shape:', X_train.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
-
-    # convert label to binary class matrix
-    Y_train = np_utils.to_categorical(Y_train, nb_classes)
-    Y_test = np_utils.to_categorical(Y_test, nb_classes)
-
-    model = Sequential()
-    model.add(Conv2D(nb_filters, nb_conv, nb_conv,
-                     border_mode='valid',
-                     # use rectifier linear units: max(0.0, x)
-                     activation='relu',
-                     input_shape=(1, 64, 64)))
-    # second convolution layer with 6 filters of size 3*3
-    model.add(Conv2D(nb_filters, nb_conv, nb_conv))
-    model.add(Activation('relu'))
-    # max pooling layer, pool size is 2*2
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    # drop out of max-pooling layer, drop out rate is 0.25
-    model.add(Dropout(0.25))
-    # flatten inputs from 2d to 1d
-    model.add(Flatten())
-    # add fully connected layer with 128 hidden units
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    # output layer with softmax
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
-    # use cross-entropy cost and adadelta to optimize paras
-    # model.compile(loss='categorical_crossentropy', optimizer='adadelta')
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adadelta(),
-                  metrics=['accuracy'])
-    # train model with bath_size=10, epoch=12
-    # set verbose=1 to show train info
-    model.fit(X_train, Y_train,
-              batch_size=batch_size,
-              epochs=nb_epoch,
-              verbose=1,
-              validation_data=(X_test, Y_test))
-    # score = model.evaluate(X_test, Y_test, verbose=0)
-    # print('Test score:', score[0])
-    # print('Test accuracy:', score[1])
-    return model
-
-
-def predict():
-    model = classfication_model()
-    (X_train, Y_train, X_valid, Y_valid, X_test, Y_test) = split_data('train_faces.pkl')
-    X_valid = X_train.reshape(X_train.shape[0], 1, 64, 64)
-    predictions = model.predict(X_valid)
-    # round predictions
-    rounded = [np.around(x) for x in predictions]
-    print(rounded)
+    def get_data(self):
+        fname = 'train_faces.pkl'
+        (X_train, Y_train, X_valid, Y_valid, X_test, Y_test) = split_data(fname)
+        X_train = X_train.reshape(X_train.shape[0], 1, 64, 64)
+        X_test = X_test.reshape(X_test.shape[0], 1, 64, 64)
+        X_valid = X_test.reshape(X_valid.shape[0], 1, 64, 64)
+        Y_train = np_utils.to_categorical(Y_train, self.nb_classes)
+        Y_test = np_utils.to_categorical(Y_test, self.nb_classes)
+        Y_valid = np_utils.to_categorical(Y_valid, self.nb_classes)
+        print('X_train shape:', X_train.shape)
+        print(X_train.shape[0], 'train samples')
+        print(X_test.shape[0], 'test samples')
+        data = {
+            'X_train': X_train,
+            'Y_train': Y_train,
+            'X_valid': X_valid,
+            'Y_valid': Y_valid,
+            'X_test': X_test,
+            'Y_test': Y_test
+        }
+        return data
+    
+    def generate_model(self):
+        data = self.get_data()
+        X_train = data.get('X_train')
+        Y_train = data.get('Y_train')
+        X_valid = data.get('X_valid')
+        Y_valid = data.get('Y_valid')
+        model = Sequential()
+        model.add(Conv2D(self.nb_filters, self.nb_conv, self.nb_conv,
+                        border_mode='valid',
+                        # use rectifier linear units: max(0.0, x)
+                        activation='relu',
+                        input_shape=(1, 64, 64)))
+        # second convolution layer with 6 filters of size 3*3
+        model.add(Conv2D(self.nb_filters, self.nb_conv, self.nb_conv))
+        model.add(Activation('relu'))
+        # max pooling layer, pool size is 2*2
+        model.add(MaxPooling2D(pool_size=(self.nb_pool, self.nb_pool)))
+        # drop out of max-pooling layer, drop out rate is 0.25
+        model.add(Dropout(0.25))
+        # flatten inputs from 2d to 1d
+        model.add(Flatten())
+        # add fully connected layer with 128 hidden units
+        model.add(Dense(128))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        # output layer with softmax
+        model.add(Dense(self.nb_classes))
+        model.add(Activation('softmax'))
+        # use cross-entropy cost and adadelta to optimize paras
+        # model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                    optimizer=keras.optimizers.Adadelta(),
+                    metrics=['accuracy'])
+        # train model with bath_size=10, epoch=12
+        # set verbose=1 to show train info
+        model.fit(X_train, Y_train,
+                batch_size=self.batch_size,
+                epochs=self.nb_epoch,
+                verbose=1,
+                validation_data=(X_valid, Y_valid))
+        return model
+    
+    def cal_score(self):
+        model = self.generate_model()
+        data = self.get_data()
+        X_test = data.get('X_test')
+        Y_test = data.get('Y_test')
+        score = model.evaluate(X_test, Y_test, verbose=0)
+        print('Test score:', score[0])
+        print('Test accuracy:', score[1])
+    
+    def predict(self):
+        model = self.generate_model()
+        X_test = self.get_data().get('X_test')
+        predictions = model.predict(X_test)
+        # round predictions
+        rounded = [np.around(x) for x in predictions]
+        print(rounded)
 
 
 def split_data(fname):
@@ -154,13 +176,8 @@ def split_data(fname):
         Y_test[i] = face_label[i*10+9]
     return (X_train, Y_train, X_valid, Y_valid, X_test, Y_test)
 
-# print(get_all_img('traindata'))
-# print(img2matrix('traindata\\yebi\\face (9).jpg'))
-# preprocessing()
-
 
 if __name__ == '__main__':
     # reshape_img()
-    
-    predict()
-    # preprocessing()
+    classify = ClassificationModel()
+    classify.predict()
