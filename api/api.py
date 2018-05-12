4fd0c0cb777a73b429f639a18b8fe9a2380eb226
Unkn0wn <unkn0wn9@outlook.com>
2018-05-12 09:11:18 +0800

    add upload_img api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 from apistar import App, Route
 from apistar import http
 import sys
+import os
+import time
 sys.path.append('../')
 
 import ineko
@@ -12,8 +14,28 @@ def getFace(query_params: http.QueryParams) -> dict:
         'response':re
     }
 
+def upload_img(request: http.Request) -> dict:
+    file_content = bytes(request.body)
+    fname = str(time.time())+'.jpg'
+    f = open('../uploads/'+fname,'wb')
+    f.write(file_content)
+    f.close
+    if(os.path.exists('../uploads/'+fname)):
+        return{
+            'response':{
+                'url': '../uploads/'+fname
+            }
+        }
+    else:
+        return{
+            'response':{
+                'error': 'upload failed'
+            }
+        }
+
 routes = [
     Route('/face', method='GET', handler=getFace),
+    Route('/images',method='POST',handler=upload_img)
 ]
 
 app = App(routes=routes)
